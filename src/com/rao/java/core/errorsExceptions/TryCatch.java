5f9ba9ba6fea9636a943bc1f2eaf4ab4bc8c7580
rydq <mandadapu.rao@directlinegroup.co.uk>
2018-04-30 16:30:16 +0100

    Rao - Try catch example

--- src/com/rao/java/core/errorsExceptions/TryCatch.java
+++ src/com/rao/java/core/errorsExceptions/TryCatch.java
@@ -5,9 +5,23 @@ package com.rao.java.core.errorsExceptions;
 public class TryCatch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int d,a;
+		d = args.length;										//if no arg then 'div by 0' or else 'ob' exception
+		try { 													// monitor a block of code.
+			a = 42 / d;
+			int c[] = { 1 };
+			c[42] = 99;
+			System.out.println("This will not be printed.");
+		}
+//		catch(Exception e) {
+//			System.out.println("Generic Exception " + e);			//This will catch all exception and makes below 2 exceptions not reachable
+//		}
+			catch (ArithmeticException e) { 						// catch divide-by-zero error
+			System.out.println("Exception: " + e);				// usefull for debug
+			System.out.println("Division by zero.");
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array index oob: " + e);
+		}
+		System.out.println("After try/catch blocks.");
 	}
-
 }
-//need to write
