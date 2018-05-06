018cfab12146a5049897d8a263dcfe59983d20a3
rydq <mandadapu.rao@directlinegroup.co.uk>
2018-05-06 04:06:53 +0100

    Rao - Thread example

--- src/com/rao/java/core/multiThreading/SingleThread.java
+++ src/com/rao/java/core/multiThreading/SingleThread.java
@@ -4,9 +4,26 @@ package com.rao.java.core.multiThreading;
 public class SingleThread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Thread t = Thread.currentThread();
+		System.out.println("Current thread: " + t);
 
+		// change the name of the thread
+		t.setName("My Thread");
+		System.out.println("After name change: " + t);
+		try {
+			for(int n = 5; n > 0; n--) {
+				System.out.println(n);
+				Thread.sleep(1000);	//use Thread ot initiation both are valid
+				t.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			System.out.println("Main thread interrupted");
+		}
+		String s=t.getName();
+		System.out.println("Thread name via getName() method ===> "+s);
 	}
 
+	/**
+	 * [main,5,main] first parameter is thread name , priority and group
+	 */
 }
-//need to write
