812159ecceb250339caa9730e223794f11ddc5f3
rydq <mandadapu.rao@directlinegroup.co.uk>
2018-05-11 00:50:03 +0100

    Rao - Thread priority example

--- src/com/rao/java/core/multiThreading/ThreadPriority.java
+++ src/com/rao/java/core/multiThreading/ThreadPriority.java
@@ -5,9 +5,56 @@ package com.rao.java.core.multiThreading;
 public class ThreadPriority {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		NewThread ob1 = new NewThread("One");
+		NewThread ob2 = new NewThread("Two");
+		NewThread ob3 = new NewThread("Three");
+		/**
+		 * isAlive is way to knoe thread status
+		 */
+		System.out.println("Thread One is alive: "
+				+ ob1.t.isAlive());
+		//Assigning priority to thread
+		ob1.t.setPriority(1);
 
+		System.out.println("Thread Two is alive: "
+				+ ob2.t.isAlive());
+		ob2.t.setPriority(4);
+
+
+//******************************important block******************************//
+		//Assign priority and print will change the default priority
+		ob3.t.setPriority(2);
+		System.out.println("Thread Three is alive: "
+				+ ob3.t.isAlive());
+		System.out.println("Thread Three : "
+				+ ob3.t);
+		System.out.println("Thread Three priority is: "
+				+ ob3.t.getPriority());
+//******************************important block******************************//
+
+
+
+		// wait for threads to finish
+		try {
+			System.out.println("Waiting for threads to finish.");
+			/**
+			 * "Join" This method waits until the thread on which it is called terminates.
+			 * Additional forms of join also available*
+			 */
+			ob1.t.join();
+			ob2.t.join();
+			ob3.t.join();
+		} catch (InterruptedException e) {
+			System.out.println("Main thread Interrupted");
+		}
+		System.out.println("Thread One is alive: "
+				+ ob1.t.isAlive());
+		System.out.println("Thread Two is alive: "
+				+ ob2.t.isAlive());
+		System.out.println("Thread Three is alive: "
+				+ ob3.t.isAlive());
+		System.out.println("Main thread exiting.");
 	}
 
+
 }
-//need to write
