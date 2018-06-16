30bd5ca0418d2ee92cba3c553ea3842717b69f1e
mandadapu <mandadapu28ster@gmail.com>
2018-06-16 17:41:20 +0100

    Rao - comment correction

--- src/com/rao/java/algorithams/KruskalMST.java
+++ src/com/rao/java/algorithams/KruskalMST.java
@@ -137,14 +137,16 @@ class Graph
     public static void main (String[] args)
     {
 
-        /* Let us create following weighted graph
-                 10
-            0--------1
-            |  \     |
-           6|   5\   |15
-            |      \ |
-            2--------3
-                4       */
+        /** Let us create following weighted graph
+         *
+         *                  10
+         *             0--------1
+         *             |  \     |
+         *            6|   5\   |15
+         *             |      \ |
+         *             2--------3
+         *                 4
+         */
         int V = 4;  // Number of vertices in graph
         int E = 5;  // Number of edges in graph
         Graph graph = new Graph(V, E);
--- src/com/rao/java/algorithams/PrimMST.java
+++ src/com/rao/java/algorithams/PrimMST.java
@@ -96,14 +96,16 @@ class MST
 
     public static void main (String[] args)
     {
-        /* Let us create the following graph
-           2    3
-        (0)--(1)--(2)
-        |    / \   |
-        6| 8/   \5 |7
-        | /      \ |
-        (3)-------(4)
-             9          */
+        /** Let us create the following graph
+         *
+         *            2    3
+         *         (0)--(1)--(2)
+         *         |    / \   |
+         *         6| 8/   \5 |7
+         *         | /      \ |
+         *         (3)-------(4)
+         *              9
+         */
         MST t = new MST();
         int graph[][] = new int[][] {{0, 2, 0, 6, 0},
                 {2, 0, 3, 8, 5},
