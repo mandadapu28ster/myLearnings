1183ecc9d99975c3e00d2cb16ee390a6f5c265f9
mandadapu <mandadapu28ster@gmail.com>
2018-06-16 15:56:43 +0100

    Rao - Hello World servlet

---
+++ src/com/rao/java/advanced/servlets/HelloWorld.java
@@ -0,0 +1,33 @@
+package src.com.rao.java.advanced.servlets;
+
+// Import required java libraries
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// Extend HttpServlet class
+public class HelloWorld extends HttpServlet {
+
+    private String message;
+
+    public void init() throws ServletException {
+        // Do required initialization
+        message = "Hello World";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    public void destroy() {
+        // do nothing.
+    }
+}
