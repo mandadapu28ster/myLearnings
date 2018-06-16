3bf836ab81706962ccfbc692a67961ab579cfae7
mandadapu <mandadapu28ster@gmail.com>
2018-06-16 15:58:55 +0100

    Rao - Hello World applet

---
+++ src/com/rao/java/advanced/applets/HelloWorld.java
@@ -0,0 +1,12 @@
+package src.com.rao.java.advanced.applets;
+
+import java.awt.*;
+import java.applet.Applet;
+
+public class HelloWorld extends Applet {
+
+    public void paint( Graphics g ) {
+        g.drawString( "Hello", 50, 50 );
+    }
+
+}
